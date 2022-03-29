39c8ac70cf2d18fe09c68c58595ab222ca27ef7d
orangeCat5 <1205382871@qq.com>
2022-03-30 01:48:50 +0800

    Site updated: 2022-03-30 01:48:50

---
+++ AJAX/server.js
@@ -0,0 +1,91 @@
+//引入express 
+const express = require('express');
+
+//2.创建应用对象
+const app = express();
+
+//3.创建路由规则
+//request 是对请求报文的封装
+//response 是对响应报文的封装
+app.get('/server', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // 设置响应体
+    response.send('HELLO AJAX');
+})
+//1、2 /server  3-json-server  
+app.all('/json-server', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    // 响应头
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    const data = {
+        name: 'lsh'
+    };
+    // 对对象进行字符串转换
+    let str = JSON.stringify(data);
+    // 设置响应体
+    response.send(str);
+})
+
+// 4. 监听端口启动服务
+app.listen(8000, () => {
+    console.log("服务已经启动, 8000 端口监听中....");
+})
+
+// 针对IE缓存
+app.get('/ie', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // 设置响应体
+    response.send('HELLO IE');
+})
+
+//延时响应
+app.get('/delay', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    setTimeout(() => {
+        response.send('延时响应');
+    }, 3000)
+    // 设置响应体
+
+})
+
+//jQuery服务
+app.all('/jquery-server', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+
+    const data = { name: '李华' };
+    response.send(JSON.stringify(data));
+})
+
+//axios服务
+app.all('/axios-server', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+
+    const data = { name: '李华' };
+    response.send(JSON.stringify(data));
+})
+
+app.all('/fetch-server', (request, response) => {
+
+    // 设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+
+    const data = { name: '李华' };
+    response.send(JSON.stringify(data));
+})
